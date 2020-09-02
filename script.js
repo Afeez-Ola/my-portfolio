fa81c29a0f582f49a8da7afc219334a1f3f440c3
Afeez-Ola <afeezbolajiola@gmail.com>
2020-09-02 03:24:00 +0100

    commit

--- script.js
+++
@@ -1,8 +0,0 @@
-let navBar = document.querySelector('nav');
-window.onscroll = function() {
-    if (window.pageYOffset > 0) {
-        navBar.classList.add('scroll')
-    } else {
-        navBar.classList.remove('scroll')
-    }
-}
